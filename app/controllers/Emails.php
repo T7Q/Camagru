d1fe1f44c90d26db0d7c14e728cbea77a6758244
T7Q <tkuumola@gmail.com>
2020-07-28 11:35:45 +0300

    feat: :sparkles: add reset  password

--- app/controllers/Emails.php
+++ app/controllers/Emails.php
@@ -5,15 +5,13 @@
 		public function __construct(){
 
 			$this->userModel = $this->model('User');
-			// $this->emailModel = $this->model('Email');
+			$this->emailModel = $this->model('Email');
 		}
 		
-		public function forgotpwd(){
-			// Load view 
-			// $this->view('emails/forgotpwd');
-		
+		public function forgotpwd(){		
 			//Check for POST
 			if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+				// echo "POST METHOD <hr>";
 				// process form
 				$data = [
 					'email' => trim($_POST['email']),
@@ -25,11 +23,9 @@
 					$data['email_err'] = 'Please enter email';
 				} else {
 					// Check email
-					if ($this->userModel->findUserByEmail($data['email'])) {
-							echo "user found <hr>";
-							// send email to user with recover link
-							$this->sendEmail($_POST['email']);
-
+					if ($this->userModel->findUserByEmail($data['email'])) {						
+							$this->sendEmail($_POST['email'], 'pwd_reset');
+							flash('reset_success', 'Password reset link has been sent');
 						} else {
 							echo "user not found <hr>";
 							$data['email_err'] = 'Account with this email does not exists';
@@ -39,62 +35,187 @@
 			// Load view
 			$this->view('emails/forgotpwd', $data);
 		}
-			
-		public function sendEmail($email){
+		
+		public function message($purpose, $url){
+			$data = [
+				'subject' => '',
+				'message' => '',
+				'header' => '',
+
+			]; 
+			if ($purpose == 'pwd_reset'){
+					$data['subject'] = 'Reset your password for '.SITENAME;
+					$data['message'] = '<p>We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.</p>';
+					$data['message'] .= '<p>Here is your password reset link: </br>';
+					$data['message'] .= '<a href="'.$url.'">'.$url.'</a></p>';
+			}
+			$data['headers'] = "From: Camagru web editing app <hiveweb7@gmail.com>\r\n";
+			$data['headers'] .= "Reply-To: <hiveweb7@gmail.com>\r\n";
+			$data['headers'] .= "Content-type: text/html\r\n";
+
+			return $data;
+
+		}
+
+		public function sendEmail($email, $purpose){
 			echo "GOT TO SEND EMAIL <hr>";
-			// $expFormat = mktime(
-			// 	date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
-			// 	);
-			// $expDate = date("Y-m-d H:i:s",$expFormat);
-			// $key = md5(2418*2 + $email);
-			// $addKey = substr(md5(uniqid(rand(),1)),3,10);
-			// $key = $key . $addKey;
+			$token = $this->emailModel->createToken($email);
+	
+			// get user id 
+			$user = $this->userModel->getUserInfo($email);
+			$username = $user->username;
 			
+			if ($this->emailModel->tokenExists($email)){
+				echo "exists <hr>";
+				// $this->emailModel->deleteToken($email);
+			} else {
+				echo "token NOT found <hr>";
+
+			}
+			
+			$url = URLROOT. "/emails/pwdreset/" . $username . "/" . $token;
+			echo "url: " . $url ."<hr>";
+			
+
 			$to = $email;
-			$subject_pwdreset = 'Reset your password for '.SITENAME;
-			$message_pwdreset = '<p>We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.</p>';
-			$message_pwdreset .= '<p>Here is your password reset link: </br>';
-			$message_pwdreset .= 'LNK HERE</p>';
-			// $message_pwdreset .= '<a href="'.$url.'">'.$url.'</a></p>';
+			// if ($title == 'pwd_reset') {
+			// 	$subject = 'Reset your password for '.SITENAME;
+			// 	$message = '<p>We received a password reset request. The link to reset your password is below. If you did not make this request, you can ignore this email.</p>';
+			// 	$message .= '<p>Here is your password reset link: </br>';
+			// 	// $message .= $url.'</p>';
+			// 	$message .= '<a href="'.$url.'">'.$url.'</a></p>';
+		
+			// }
 
-			$headers = "From: Camagru web editing app <hiveweb7@gmail.com>\r\n";
-			$headers .= "Reply-To: <hiveweb7@gmail.com>\r\n";
-			$headers .= "Content-type: text/html\r\n";
+			// $headers = "From: Camagru web editing app <hiveweb7@gmail.com>\r\n";
+			// $headers .= "Reply-To: <hiveweb7@gmail.com>\r\n";
+			// $headers .= "Content-type: text/html\r\n";
+		
 
-			mail($to, $subject_pwdreset, $message_pwdreset, $headers);
+			$message = $this->message($purpose, $url);
+			mail($to, $message['subject'], $message['message'], $message['headers']);
+			// mail($to, $subject, $message, $headers);
+		}
+
+
+		public function validateNewPwd(&$data) {
+			echo "got to function <hr>";
+			if (empty($data['password'])) {
+				$data['password_err'] = 'Please enter password';
+			} elseif (strlen($data['password']) < 6) {
+				$data['password_err'] = 'Password must be at least 6 characters';
+			}
+
+			// Validate Confirm Password
+			if (empty($data['confirm_password'])) {
+				$data['confirm_password_err'] = 'Please confirm password';
+			} else {
+				if ($data['password'] != $data['confirm_password']) {
+					$data['confirm_password_err'] = 'Passwords do not match';
+				}
+				// Make sure errors are empty
+				if (empty($data['password_err']) && empty($data['confirm_password_err'])) {
+					return true;
+					
+				} else {
+					return false;
+				}
+				return false;
+			}
 		}
 		
 
+
+		public function pwdreset ($username = '', $token = '') {
+			session_start();
+			if ($username != ''){
+				$_SESSION['tkn_user'] = $username;
+			}
+			// validate token
+			if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+				$this->view('emails/pwdreset');
+				
+				if ($this->emailModel->isValidToken($username, $token)) {
+					flash('token', 'Please enter your new password');
+				} else {
+					echo "token invalid <hr>";
+				}
+			}
+
+			// Check for POST
+			if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+				// process form
+				$data = [
+					'password' => trim($_POST['password']),
+					'confirm_password' => trim($_POST['confirm_password']),
+					'password_err' => '',
+					'confirm_password_err' => '',
+				];
+
+				// Validate Password
+				if ($this->validateNewPwd($data)){
+					// Hashed pasword
+					$data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+					// Update password data in database
+					if ($this->userModel->pwdUpdate($data['password'], $_SESSION['tkn_user'])){
+						unset($_SESSION['tkn_user']);
+						flash('register_success', 'Your password was successfully updated, please log in');
+						redirect('users/login');
+					} else {
+						// 	echo "failure to update <hr>";
+					}
+				} else {
+					$this->view('emails/pwdreset', $data);
+				}
+				// print_r($data);
+
+				// if (empty($data['password'])) {
+				// 	$data['password_err'] = 'Please enter password';
+				// } elseif (strlen($data['password']) < 6) {
+				// 	$data['password_err'] = 'Password must be at least 6 characters';
+				// }
+
+				// Validate Confirm Password
+				// if (empty($data['confirm_password'])) {
+				// 	$data['confirm_password_err'] = 'Please confirm password';
+				// } else {
+				// 	if ($data['password'] != $data['confirm_password']) {
+				// 		$data['confirm_password_err'] = 'Passwords do not match';
+				// 	}
+				// 	// Make sure errors are empty
+				// 	if (empty($data['password_err']) && empty($data['confirm_password_err'])) {
+				// 		// Validated
+
+				// 		// Hashed pasword
+				// 		$data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+				// 		if ($this->userModel->pwdUpdate($data['password'], $_SESSION['tkn_user'])){
+				// 			unset($_SESSION['tkn_user']);
+				// 			flash('register_success', 'Your password was successfully updated, please log in');
+				// 			redirect('users/login');
+				// 		} else {
+				// 			// 	echo "failure to update <hr>";
+				// 		}
+						
+				// 	} else {
+				// 		$this->view('emails/pwdreset', $data);
+				// 	}
+				// 	$this->view('emails/pwdreset', $data);
+				// }
+			} else {
+				// Init data
+				$data = [
+					'password' => '',
+					'confirm_password' => '',
+					'password_err' => '',
+					'confirm_password_err' => ''
+				];
+
+				// Load view
+				$this->view('emails/pwdreset', $data);
+			}
+		}
+
 	}	
 ?>
-	<!-- // // Check for username
-	// if ($this->emailModel->isValidToken($data['token'])){
-	// 	// Token exists
-	// 	echo "token VALID <hr>";
-	// 	$this->view('users/register', $data);
-	// } else {
-	// 	// Token expired or does not exist
-	// 	echo "toke expired <hr>";
-	// 	$data['token_err'] = "Token not found, resend a password".URLROOT ."/users/pwdReset";
-	// }
-// }
-// }
-
-// public function sendEmail($email_type, $data){
-// 	$url = "test";
-// 	$subject_pwdreset = 'Reset your password for '.SITENAME;
-// 	$message_pwdreset = '<p>We received a password reset request. The link to reset your password is below. If you did not make this requiest, you can ignore this email.</p>';
-// 	$message_pwdreset .= '<p>Here is your password reset link: </br';
-// 	$message_pwdreset .= 'LNK HERE</p>';
-// 	// $message_pwdreset .= '<a href="'.$url.'">'.$url.'</a></p>';
-
-// 	$headers = "From: Camagru web editing app <hiveweb7@gmail.com>\r\n";
-// 	$headers .= "Reply-To: <hiveweb7@gmail.com>\r\n";
-// 	$headers .= "Content-type: text/html\r\n";
-
-// 	mail($to, $subject, $message, $headers){
-// 	redirect('users/pwdreset');
-// 	}
-// 	// Load view
-// 	$this->view('emails/forgotpwd', $data);
-// } -->
--- app/models/Email.php
+++ app/models/Email.php
@@ -8,16 +8,64 @@
 			$this->database = new Db;
 		}
 
-		// Find user by email
+		// Create token (and add it to database) for account activation or password reset
+		public function createToken($email) {			
+			$token = bin2hex(random_bytes(32));
+
+			// add token to database
+			$this->database->query('UPDATE `user` SET token=:token WHERE email=:email');
+			$this->database->bind(':token', $token);
+			$this->database->bind(':email', $email);
+			// echo "after database" . $token . "<hr>";
+			
+			$this->database->execute();
+			// echo "updated database <hr>";
+			// $this->updateToken($token, $email);
+			
+			return $token;
+		}
+
+		// Validate token
+		public function isValidToken($username, $token) {
+
+			// add token to database
+			$this->database->query('SELECT * FROM `user` WHERE username=:username');
+			$this->database->bind(':username', $username);
+
+			$row = $this->database->single();
+			if ($row > 0) {
+				if($row->token == $token){
+					// echo "token match <hr>";
+					return true;
+				} else {
+					// echo "token does not match <hr>";
+					return false;
+				}
+			} else {
+				// echo "user does not exit <hr>";
+				return false;
+			}
+			// echo "after database" . $token . "<hr>";
+			
+			$this->database->execute();
+			// echo "updated database <hr>";
+			// $this->updateToken($token, $email);
+			
+			return $token;
+		}
+
+
+		// Check if token exists
 		public function tokenExists($email)
 		{
 			// check if token from this user already exists
-			$this->database->query('SELECT * FROM pwdreset WHERE email = :email');
+			$this->database->query('SELECT * FROM user WHERE email = :email');
 			$this->database->bind(':email', $email);
 			
 			$row = $this->database->single();
 			// check row
-			if ($this->database->rowCount() > 0) {
+			// if ($this->database->rowCount() > 0) {
+			if ($row->token != NULL) {
 				return true;
 			} else {
 				return false;
