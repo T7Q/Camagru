d2e54d7b82c1ab84742e977c91042f7aaecafe80
T7Q <tkuumola@gmail.com>
2020-08-10 23:39:20 +0300

    style: :art: update colors

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -3,36 +3,28 @@
 // no JS? -->
 
 <?php require './app/views/inc/header.php'; ?>
-	<!-- <div class="row full-height"> -->
-		<div class="row half-height text-center div1">
-			<div class="container my-auto">
-			<div>
-				<button style="background-color: pink" id="btn2">CLICK ME</button>
-				<button id="btn3">CLICK ME2</button>
-			</div> 
-				
 
-				<div style="margin-top: 25%">
-					<div class="row justy_center">
-						<h1> CAMAGRU</h1>
-					</div>
-					<div class="col-4 aligntocenter">
-					<!-- <div class="col-5 center-block aligntocenter"> -->
-						<div class="embed-responsive embed-responsive-1by1 text-center">
-							<div id="baloon" class="circleshape embed-responsive-item bg-primary">
-							</div>
+	<div class="row half-height text-center">
+		<div class="container my-auto">
+			<div >
+				<div class="row justy_center">
+					<h1> CAMAGRU</h1>
+				</div>
+				<div class="col-4 aligntocenter">
+					<div class="embed-responsive embed-responsive-1by1 text-center">
+						<!-- <div id="baloon" class="circleshape embed-responsive-item bg-primary"> -->
+						<div id="baloon" class="circleshape embed-responsive-item">
 						</div>
 					</div>
-					<div class="container text-center my-auto">
-						<h1>ENHANCE YOUR IMAGES</h1>
-					</div>
+				</div>
+				<div class="container text-center my-auto">
+					<h1>ENHANCE YOUR IMAGES</h1>
 				</div>
 			</div>
-        </div>
-		
-		<div class="row half-height colorgrey div2">
-			
 		</div>
+	</div>
+	<div class="row half-height colorgrey">
+	</div>
 
-</div>
+	
 <?php require './app/views/inc/footer.php'; ?>
--- public/js/home.js
+++ public/js/home.js
@@ -1,47 +1,13 @@
 // not logged in see home page 
 // logged in have updated context
 
-function favorite() {
-	var parent = this.parentElement;
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', '../app/views/pages/new.php', true);
-	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-	xhr.onreadystatechange = function () {
-	  console.log('readyState: ' + xhr.readyState);
-	  if(xhr.readyState == 2) {
-		parent.innerHTML = 'Loading...';
-	  }
-	  if(xhr.readyState == 4 && xhr.status == 200) {
-		parent.innerHTML = xhr.responseText;
-		console.log(xhr.responseText);
-	  }
-	}
-	xhr.send("id=" + parent.id);
+const colors = ["#0000ff", "#cc3300", "#ff5011", "#ffc000", "#3eba3a", "#59f3cd", "#0099ff"];
+let colorIndex = 0;
+window.onload = function (){
+	setInterval(function(){
+		console.log("color active");
+		colorIndex++;
+		colorIndex  = (colorIndex == 7) ? 0 : colorIndex;
+		document.getElementById("baloon").style.backgroundColor = colors[colorIndex];
+	}, 3000);
   }
-
-  let button = document.getElementById("home_test");
-  button.addEventListener("click", favorite);
-
-  document.getElementById("baloon").addEventListener(('click'), function(e) {
-	console.log("click on baloon");
-});
-	data = 'update';
-	request  = new Request ({
-	url        : "pages/color.php",
-	method     : 'POST',
-	handleAs   : 'json',
-	parameters : { data : JSON.stringify(data) },
-	onSuccess  : function(res) {
-					if (res['valid']) {
-						new_color = res['color']
-						document.getElementById("baloon").style.color = new_color;
-						console.log("got to if");
-					}
-					else
-						console.log("got to else");
-	},
-	onError    : function(status, res) {
-					console.log("error occured");
-	}
-});
