46cb2e6c2e5bc3c4cb137af32d7795038b216211
T7Q <tkuumola@gmail.com>
2020-08-19 13:13:49 +0300

    feat: :zap: add delete img from the server

--- app/controllers/Images.php
+++ app/controllers/Images.php
@@ -3,6 +3,7 @@
 class Images extends Controller {
 	public function __construct() {
 		$this->imageModel = $this->model('Image');
+		$this->galleryModel = $this->model('Gallery');
 	}
 	public function create(...$param){
 		if ($this->isAjaxRequest()) {
@@ -101,5 +102,48 @@ class Images extends Controller {
 		}
 
 	}
+
+	public function delete() {
+		if ($this->isAjaxRequest()) {
+			if (isset($_POST['data'])) {
+				$data = json_decode($_POST['data'], true);
+				$temp = explode("del_img", $data['id_image']);
+				$id_image = $temp[1];
+
+				$id_user = (isset($_SESSION['user_id'])) ? $_SESSION['user_id'] : 0 ;
+				$json['loggedIn'] = (isset($_SESSION['user_id'])) ? true: false ;
+				if (isset($_SESSION['user_id'])){
+					if($this->galleryModel->imageExists($id_image)){
+						$image_owner = $this->galleryModel->imageOwner($id_image);
+						$json['valid'] = false;
+						// $json['message'] = $image_owner->id_user;
+						if ($id_user === $image_owner->id_user){
+							$temp = $this->imageModel->getImgPath($id_image);
+							$this->imageModel->deleteImgDb($id_image);
+							$json['valid'] = true;
+							$json['message'] = "Successfully deleted";
+							unlink(APPPATH . '/' . $temp->path); // Delete img from server
+						} else {
+							$json['valid'] = false;
+							$json['message'] = "You can't delete other user images";
+						}
+					} else {
+						$json['valid'] = false;
+						$json['message'] = "Image does not exist in your gallery";
+					}
+				} else {
+					$json['valid'] = false;
+					$json['message'] = "You need to be logged in to remove image";
+				}
+			} else {
+				$json['valid'] = false;
+				$json['message'] = "Oops, something went wrong getting images for Gallery";
+			}
+			echo json_encode($json);
+		} else {
+			$this->view('pages/error');
+		}
+	}
+
 }
 ?>
